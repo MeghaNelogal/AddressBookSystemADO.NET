f0e52bff35892e3d9195a640eaf0504d97671a79
Your Name <you@example.com>
2022-09-08 16:51:05 +0530

    [Megha] Add: deleted  existing contact

--- AddressBookSystemADO.NET/AddressBookSystemADO.NET/Program.cs
+++ AddressBookSystemADO.NET/AddressBookSystemADO.NET/Program.cs
@@ -17,8 +17,12 @@
             AddNewContactDetails();
 
             //UC4                                                      // Zip,   FirstName, LastName
-            Console.WriteLine(addressBookManagement.EditContactUsingName("577101", "Ekta", "Kapoor") ? "Update Record successfully\n" : "Update failed");
-        }
+            Console.WriteLine(addressBookManagement.EditContactUsingName("222105", "megha", "nelogal") ? "Update Record successfully\n" : "Update failed");
+
+            //UC5
+            Console.WriteLine(addressBookManagement.DeleteContactUsingName("megha", "nelogal") ? "Record Deleted successfully\n" : "Delete failed");
+        
+    }
         //UC3:Ability to insert new Contacts to Address Book
         public static void AddNewContactDetails()
         {
